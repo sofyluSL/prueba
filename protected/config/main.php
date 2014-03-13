e3eebb9df4864543351bc7584f6a7d1fc6021ee9
Sofy <Sofy@sofylu>
2014-03-12 22:11:23 -0500

    Merge origin/master
    Conflicts:
    	css/form.css
    	css/main.css
    	css/screen.css
    	index.php
    	protected/config/main.php
    	protected/views/layouts/column1.php
    	protected/views/layouts/column2.php
    	protected/views/layouts/main.php
    	protected/views/site/contact.php
    	protected/views/site/error.php
    	protected/views/site/index.php
    	protected/views/site/login.php
    	protected/views/site/pages/about.php

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'My Web Application',
+	'name'=>'Nutrición',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
@@ -37,29 +37,29 @@ return array(
 			'allowAutoLogin'=>true,
 		),
 		// uncomment the following to enable URLs in path-format
-		/*
+		
 		'urlManager'=>array(
 			'urlFormat'=>'path',
+                        'showScriptName' => false,
 			'rules'=>array(
 				'<controller:\w+>/<id:\d+>'=>'<controller>/view',
 				'<controller:\w+>/<action:\w+>/<id:\d+>'=>'<controller>/<action>',
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		*/
-		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
+		
+//		'db'=>array(
+//			'connectionString' => 'mysql:'.dirname(__FILE__).'/../data/testdrive.db',
+//		),
 		// uncomment the following to use a MySQL database
-		/*
+		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=localhost;dbname=nutricion',
 			'emulatePrepare' => true,
 			'username' => 'root',
 			'password' => '',
 			'charset' => 'utf8',
 		),
-		*/
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
